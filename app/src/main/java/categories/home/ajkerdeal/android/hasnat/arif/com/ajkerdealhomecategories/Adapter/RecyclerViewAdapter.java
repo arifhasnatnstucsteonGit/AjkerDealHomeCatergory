c458417ba88ebdbe7c2e3590721f67111ee5c354
Arif Hasnat <arifhasnat.info@gmail.com>
2015-12-23 17:50:48 +0600

    level zero

---
+++ app/src/main/java/categories/home/ajkerdeal/android/hasnat/arif/com/ajkerdealhomecategories/Adapter/RecyclerViewAdapter.java
@@ -0,0 +1,15 @@
+package categories.home.ajkerdeal.android.hasnat.arif.com.ajkerdealhomecategories.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by arifhasnat on 12/23/15.
+ */
+public class RecyclerViewAdapter extends RecyclerView.ViewHolder {
+    public RecyclerViewAdapter(View itemView) {
+        super(itemView);
+    }
+
+
+}
